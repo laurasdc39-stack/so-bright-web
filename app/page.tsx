13b20604cfe8979762dca57708ef7db932ce14c2
laurasdc39-stack <laurasdc39@gmail.com>
2026-05-13 12:21:30 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,35 @@
-"use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
+export default function Home() {
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1
+        style={{
+          color: "#875F89",
+          fontSize: "60px",
+          marginBottom: "20px",
+        }}
+      >
+        So Bright
+      </h1>
+
+      <p
+        style={{
+          fontSize: "24px",
+          color: "#333",
+        }}
+      >
+        Blanqueamiento dental profesional
+      </p>
     </main>
   );
 }
